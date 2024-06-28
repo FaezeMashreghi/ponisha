2b7de260b837add6279f5f88d519f4210ffb05fb
f.mashreghi <f.mashreghi@azki.com>
2024-06-28 14:53:00 +0330

    feat(components):implement ChecBoxCard

---
+++ shop/src/components/CheckBoxCard/index.js
@@ -0,0 +1,37 @@
+import { Card, CardActionArea, Checkbox } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+export const CheckBoxCard = ({
+  label,
+  description,
+  checked,
+  id,
+  onCardCheck,
+  rightLabel,
+}) => {
+  return (
+    <Card>
+      <CardActionArea
+        onClick={() => onCardCheck(id)}
+        sx={{
+          p: 1,
+          border: checked ? "1px solid purple" : "",
+          backgroundColor: checked ? "#fbf7fc" : "white",
+          display: "flex",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Checkbox color="secondary" checked />
+        <div>
+          <Typography variant="subtitle1">{label}</Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {description}
+          </Typography>
+        </div>
+        <Typography variant="subtitle1" ml="auto">
+          {rightLabel}
+        </Typography>
+      </CardActionArea>
+    </Card>
+  );
+};
