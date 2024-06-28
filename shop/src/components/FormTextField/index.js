2a851d5f14a58ce6c3a02d4f8584d4cea4b91224
f.mashreghi <f.mashreghi@azki.com>
2024-06-28 14:08:20 +0330

    fix(components):fix error message of textField

--- shop/src/components/FormTextField/index.js
+++ shop/src/components/FormTextField/index.js
@@ -1,27 +1,23 @@
 import { Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 
-export const FormInputText = ({ name, control, label }) => {
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({
-                         field: { onChange, value },
-                         fieldState: { error },
-                         formState,
-                     }) => (
-                <TextField
-                    helperText={error ? error.message : null}
-                    size="small"
-                    error={!!error}
-                    onChange={onChange}
-                    value={value}
-                    fullWidth
-                    label={label}
-                    variant="outlined"
-                />
-            )}
+export const FormTextField = ({ name, control, label, className }) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          helperText={error?.message}
+          size="small"
+          error={!!error}
+          fullWidth
+          label={label}
+          variant="outlined"
+          className={className}
         />
-    );
+      )}
+    />
+  );
 };
